6d93d34605a29100fca5b7f93605bce4b65eadb4
Александр Слав <Aleksslav@gmail.com>
2021-01-21 11:09:27 +0300

    Подготовка проекта к первой проверке

---
+++ build/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var navMain = document.querySelector('.header-nav');
+var navToggle = document.querySelector('.header-nav__toggle');
+
+navMain.classList.remove('header-nav--no-js');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('header-nav--closed')) {
+    navMain.classList.remove('header-nav--closed');
+    navMain.classList.add('header-nav--opened');
+  } else {
+    navMain.classList.add('header-nav--closed');
+    navMain.classList.remove('header-nav--opened');
+  }
+});
