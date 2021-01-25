815e2289af155103a73a2d12b28712c5cd512033
Александр Слав <Aleksslav@gmail.com>
2021-01-25 19:59:43 +0300

    Внес правки

--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,38 @@
 'use strict';
 
-var navMain = document.querySelector('.header-nav');
-var navToggle = document.querySelector('.header-nav__toggle');
+(function () {
 
-navMain.classList.remove('header-nav--no-js');
+  var navMain = document.querySelector('.header-nav');
+  var navToggle = document.querySelector('.header-nav__toggle');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('header-nav--closed')) {
-    navMain.classList.remove('header-nav--closed');
-    navMain.classList.add('header-nav--opened');
-  } else {
-    navMain.classList.add('header-nav--closed');
-    navMain.classList.remove('header-nav--opened');
+  navMain.classList.remove('header-nav--no-js');
+
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('header-nav--closed')) {
+      navMain.classList.remove('header-nav--closed');
+      navMain.classList.add('header-nav--opened');
+    } else {
+      navMain.classList.add('header-nav--closed');
+      navMain.classList.remove('header-nav--opened');
+    }
+
+  });
+
+  var links = document.querySelectorAll('.site-list__item');
+  var addSlowScroll = function (link) {
+    link.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      var id = link.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  };
+
+  for (var i = 0; i < links.length; i++) {
+    addSlowScroll(links[i]);
   }
-});
+
+})();
