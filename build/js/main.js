05132ac498a337e1dd52e7a65ea1146d9edd1a39
Александр Слав <Aleksslav@gmail.com>
2021-02-03 10:50:35 +0300

    Правки

--- build/js/main.js
+++ build/js/main.js
@@ -1,38 +1,6 @@
 'use strict';
 
 (function () {
-  const body = document.querySelector(`.page-body`);
-  let header;
-  let navMain;
-  let navToggle;
-
-  const existsInPage = (node) => {
-    return body.contains(node);
-  };
-
-  if (existsInPage(body.querySelector(`.page-header`))) {
-    header = body.querySelector(`.page-header`);
-
-    if (existsInPage(body.querySelector(`.header-nav`))) {
-      navMain = header.querySelector(`.header-nav`);
-
-      if (existsInPage(navMain.querySelector(`header-nav__toggle`))) {
-        navToggle = navMain.querySelector(`header-nav__toggle`);
-      }
-    }
-  }
-
-  window.util = {
-    body,
-    header,
-    navMain,
-    navToggle,
-    existsInPage
-  };
-
-})();
-
-(function () {
 
   var navMain = document.querySelector('.header-nav');
   var navToggle = document.querySelector('.header-nav__toggle');
@@ -40,17 +8,17 @@
 
   navMain.classList.remove('header-nav--no-js');
 
-  navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('header-nav--closed')) {
-      navMain.classList.remove('header-nav--closed');
-      navMain.classList.add('header-nav--opened');
-      body.classList.add('overflow');
-    } else {
-      navMain.classList.add('header-nav--closed');
-      navMain.classList.remove('header-nav--opened');
-      body.classList.remove('overflow');
-    }
 
+  if (document.contains(navMain) && document.contains(navToggle)) {
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('header-nav--closed')) {
+        navMain.classList.remove('header-nav--closed');
+        navMain.classList.add('header-nav--opened');
+        body.classList.add('overflow');
+      } else {
+        navMain.classList.add('header-nav--closed');
+        navMain.classList.remove('header-nav--opened');
+        body.classList.remove('overflow');
+      }
   });
-})();
-
+}})();
